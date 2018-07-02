0b44729528ceee06b775a7d5bf4eb68334135a36
Nick Panteleeff <drnix@chess-online.com>
2018-07-02 17:14:00 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,37 @@
-var path = require("path");
-var webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map', 
+
     entry: {
         react: ['react', 'react-dom'],
-        app: ["./built/index.js"],
-        tests: ["./built/test/index.js"],
+        app: ["./src/index.ts"],
+        tests: ["./src/test/index.ts"]
     },
 
     output: {
         libraryTarget: "umd",
         library: "onix",
         path: path.join(__dirname, "public"),
-		filename: "pos-builder.[name].js"
-    },
-
-    plugins:[
-        new webpack.optimize.CommonsChunkPlugin({ name: "react" }),
-    ],
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        extensions: [".webpack.js", ".web.js", ".js"]
+        filename: "pos-builder.[name].js"
     },
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, enforce: "pre", loader: "source-map-loader" }
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
     },
+
+    devServer: {
+      contentBase: './public'
+    }
 };

ac936872ced4f9c96c8e09085be90648ad9549c1
Nick Panteleeff <drnix@chess-online.com>
2019-10-15 19:55:49 +0300

    Work

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -2,11 +2,23 @@ import PATHS from '../paths';
 
 import gulp from 'gulp';
 import ts from 'gulp-typescript';
+import sourcemaps from 'gulp-sourcemaps';
+import merge from 'merge2';
 
-const tsProject = ts.createProject('tsconfig.json');
+const tsProj = ts.createProject('tsconfig.json');
 
 export default function scripts() {
-	return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest(PATHS.build.scripts));
+        const reporter = ts.reporter.fullReporter();
+
+        const tsResult = gulp.src(['src/js/**/*.ts', 'src/js/**/*.tsx'])
+            .pipe(sourcemaps.init())
+            .pipe(tsProj(reporter));
+
+        return merge([
+            tsResult.dts
+                .pipe(gulp.dest(PATHS.build.scripts)),
+            tsResult.js
+                .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest(PATHS.build.scripts))
+        ]);
 }
